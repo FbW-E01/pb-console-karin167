94cb7453157947c5b53fa1d0ac62c989e0826c59
karin yahud <karin167bz@gmail.com>
2021-03-29 13:52:23 +0200

    task

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("this is an exercise in console logging" );
+
+let  variable = "This is the value";
+console.log(variable);
+
+let firstName = "karin";
+console.log("First name:" +" "+ firstName);
+
+let secondName = "Yahud";
+console.log("Second name:" +" "+ secondName);
+
+let city = "Berlin";
+console.log("City:" +" "+ city);
+
+let cuntry= "Germany";
+console.log("Cuntry:" +" "+cuntry);
+
+let favouriteColor= "Green";
+console.log("favourite color :" +" "+favouriteColor);
+
+let favouriteFood= "chnitzel";
+console.log(" My favourite food is :" +" "+favouriteFood);
+
+
+let age= "26";
+console.log("Hello, my name is:" + firstName + " " + "i am"+" " +age +" "+"years old");
+
+
+
